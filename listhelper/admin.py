9b082bd3285f2e90df0a268c27190dc82450fe73
simon <hallo@simonkuhlo.de>
2024-06-04 14:26:40 +0200

    init

--- listhelper/admin.py
+++ listhelper/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from listhelper import models
 
 # Register your models here.
+admin.site.register(models.ChecklistItem)
+admin.site.register(models.File)
--- listhelper/models.py
+++ listhelper/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+
+class File(models.Model):
+    name = models.CharField(max_length=100)
+    path = models.CharField(max_length=300)
+
+class ChecklistItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    relatedFile = models.ForeignKey(File, on_delete=models.SET_NULL, null=True, blank=True)
